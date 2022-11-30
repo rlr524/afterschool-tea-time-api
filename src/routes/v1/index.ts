1fba66e45858a3d5bc1e3e93b7481c7c14920586
Rob Ranf <rob@emiyaconsulting.com>
2022-11-30 10:53:17 -0800

    Adding validation to customer update route and refactoring unprotected routes
    Signed-off-by: Rob Ranf <rob@emiyaconsulting.com>

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,21 +4,30 @@ import { body, validationResult } from "express-validator";
 const r = Router();
 
 /**
- * @description - All product route handlers
+ * Account vs Customer
+ * The /account routes reference the CustomerAccount table and are required for shopping on HTT. Creating a
+ * customer account via the POST /account route (in server.ts as it's not a protected route) creates an account
+ * and a customer profile, so creates entries in the CustomerAccount, CustomerProfile and CustomerDetail tables.
+ * The /customer routes reference the CustomerProfile and CustomerDetail tables and are additional shopper
+ * profiles linked to a single customer account (think Netflix profiles) in that they all sign in with the same
+ * login and password, but have their own shopping profiles including limits that can be managed only by the
+ * account owner with a specific PIN. This allows, for example, parents to allow college aged children to
+ * make purchases while away at school on the parent's account.
  */
 
-r.get("/product", (req, res) => {
-	res.json({ message: "Hello from the /product route" });
-});
-r.get("/product/:id", () => {
-	//
-});
+/**
+ * @description - Senpai user roduct route handlers
+ */
+
+// Available only to Senpai users ADMIN, PRODMGMT
 r.put("/product/:id", () => {
 	//
 });
+// Available only to Senpai users ADMIN, PRODMGMT
 r.post("/product", () => {
 	//
 });
+// Available only to Senpai users ADMIN, PRODMGMT
 r.delete("/product/:id", () => {
 	//
 });
@@ -27,12 +36,15 @@ r.delete("/product/:id", () => {
  * @description - All customer route handlers
  */
 
+// Available only to Senpai users ADMIN, CUSTSERV
 r.get("/customer", () => {
 	//
 });
+// Available only to authenticated customer and Senpai users ADMIN, CUSTSERV
 r.get("/customer/:id", () => {
 	//
 });
+// Available only to authenticated customer and Senpai users ADMIN, CUSTSERV
 r.put(
 	"/customer/:id",
 	body("lastname").isString(),
@@ -55,9 +67,30 @@ r.put(
 		}
 	}
 );
-r.post("/customer", () => {
-	//
-});
+// Available only to authenticated customer and Senpai users ADMIN, CUSTSERV
+r.post(
+	"/customer",
+	body("lastname").isString(),
+	body("firstname").isString(),
+	body("middleinitial").isString(),
+	body("email").isString(),
+	body("customerPhone").isString(),
+	body("customerShipAddLn1").isString(),
+	body("customerShipAddLn2").isString(),
+	body("customerShipAddCity").isString(),
+	body("customerShipAddState").isString(),
+	body("customerShipAddZIP").isString(),
+	body("customerDOB").isDate(),
+	(req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			res.status(400);
+			res.json({ errors: errors.array });
+		}
+	}
+);
+// Available only to authenticated customer and Senpai users ADMIN, CUSTSERV
 r.delete("/customer/:id", () => {
 	//
 });
@@ -66,15 +99,19 @@ r.delete("/customer/:id", () => {
  * @description - All customer account route handlers
  */
 
+// Available only to Senpai users ADMIN, CUSTSERV
 r.get("/account", () => {
 	//
 });
+// Available only to authenticated customer and Senpai users ADMIN, CUSTSERV
 r.get("/account/:id", () => {
 	//
 });
+// Available only to authenticated customer and Senpai users ADMIN, CUSTSERV
 r.put("/account/:id", () => {
 	//
 });
+// Available only to authenticated customer and Senpai users ADMIN, CUSTSERV
 r.delete("/account/:id", () => {
 	//
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { createCustomerAccount, signin } from "./handlers/customer";
 import { createUserAccount, userSignin } from "./handlers/user";
 import express from "express";
+import { body, validationResult } from "express-validator";
 import router from "./routes/v1";
 import { protect } from "./middleware";
 import morgan from "morgan";
@@ -29,9 +30,56 @@ app.get("/", (req, res) => {
 app.use("/api/v1/", protect, router);
 
 // Handle these routes separate from all other routes to avoid the protect middleware.
-app.post("/account", createCustomerAccount);
-app.post("/signin", signin);
+// TODO: Refactor the express-validator functions into a separate middleware
+app.post(
+	"/account",
+	body("login").isString(),
+	body("password").isString(),
+	body("lastname").isString(),
+	body("firstname").isString(),
+	body("middleinitial").isString(),
+	body("email").isString(),
+	body("customerPhone").isString(),
+	body("customerShipAddLn1").isString(),
+	body("customerShipAddLn2").isString(),
+	body("customerShipAddCity").isString(),
+	body("customerShipAddState").isString(),
+	body("customerShipAddZIP").isString(),
+	body("customerDOB").isDate(),
+	(req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			res.status(400);
+			res.json({ errors: errors.array() });
+		}
+		createCustomerAccount;
+	}
+);
+
+app.post(
+	"/signin",
+	body("login").isString(),
+	body("password").isString(),
+	(req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			res.status(400);
+			res.json({ errors: errors.array() });
+		}
+		signin;
+	}
+);
+
 app.post("/sensei-account", createUserAccount);
 app.post("/sensei-signin", userSignin);
+app.get("/product", (req, res) => {
+	res.json({ message: "Hello from the /product route" });
+});
+// Open route
+app.get("/product/:id", () => {
+	//
+});
 
 export default app;
