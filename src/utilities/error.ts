2bcbb673a5e1b91ed06068fb35532e5f817758af
Rob Ranf <rob@emiyaconsulting.com>
2023-04-07 21:39:37 -0700

    fixed unused var error in error.ts

--- src/utilities/error.ts
+++ src/utilities/error.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler = (err, req, res, next) => {
 	if (err.type === "auth") {
 		res.status(401).json({ message: "unauthorized" });
